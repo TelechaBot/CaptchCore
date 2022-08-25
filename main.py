cad25e6cb0cde773d923ca364758bd5beb1891e3
sudoskys <coldlando@hotmail.com>
2022-08-25 15:05:00 +0800

    add:伟大的作品

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 # @Version    ：1
 import math
 import random
+import time
 
 special_angle = [30, 60, 90, 120, 150, 180]
 
@@ -34,7 +35,7 @@ class radius(object):
     def create(self):
         lena = (random.randint(5, 20) * 2)
         r = (random.randint(5, 10) * 2)
-        Q = f"一个扇形弧长为{lena}，半径为{r}，求其面积！四舍五入，只答出数字"
+        Q = f"一个扇形弧长为{lena}，半径为{r}，求其面积为多少π！（四舍五入，只答出数字）"
         A = (lena * r) / 2
         return Q, round(A)
 
@@ -51,7 +52,7 @@ class find_volume_cone(object):
         lena = (random.randint(1, 28) * 3)
         h = (random.randint(1, 20) * 2)
 
-        Q = f"一个圆锥的底面积为{lena}π，高为{h}，求其体积!(四舍五入，只答出数字部分！)"
+        Q = f"一个圆锥的底面积为{lena}π，高为{h}，求其体积为多少π!(四舍五入，只答出数字部分！)"
         A = (lena * h) / 3
         return Q, round(A)
 
@@ -67,7 +68,7 @@ class find_ball_cone(object):
     def create(self):
         # lena = (random.randint(1, 30) * 3)
         r = (random.randint(1, 14) * 3)
-        Q = f"一个球的半径为{r}，求其体积!(四舍五入，只答出数字部分！)"
+        Q = f"一个球的半径为{r}，求其体积是多少π!(四舍五入，只答出数字部分！)"
         A = 4 / 3 * r ** 3
         return Q, round(A)
 
@@ -170,7 +171,7 @@ class biological_gene(object):
 
     @property
     def difficulty(self):
-        return 9
+        return 7
 
     def create(self):
         num1 = (random.randint(7, 21) * 4)
@@ -187,13 +188,15 @@ class biological_gene(object):
 
 
 class Importer(object):
-    def __init__(self):
+    def __init__(self, something=time.time()):
+        self.samples = something
         self.items = [{"diff": parabola_2().difficulty, "obj": parabola_2()},
                       {"diff": radius().difficulty, "obj": parabola()},
                       {"diff": find_volume_cone().difficulty, "obj": find_volume_cone()},
                       {"diff": find_ball_cone().difficulty, "obj": find_ball_cone()},
                       {"diff": gravity_work().difficulty, "obj": gravity_work()},
                       {"diff": binary_first_equation().difficulty, "obj": binary_first_equation()},
+                      {"diff": biological_gene().difficulty, "obj": biological_gene()},
                       ]
 
     def pull(self, difficulty_min=1, difficulty_limit=5):
@@ -209,8 +212,17 @@ class Importer(object):
 
 # print(biological_gene().create())
 
+###########################
+# 如果需要创建不重复对象
+# import time
+# print(Importer(time.time()).pull().create())
+# print(Importer(time.time()).pull().create())
+# print(Importer(time.time()).pull().create())
+#########################
+
+
 #
-# some = Importer().pull()
+# some =
 # print(some.create())
 # print(some.create()[0])
 # print(some.create()[1])
